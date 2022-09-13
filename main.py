e42aa3765565fb624ef53ddf8674989f88adb3da
zielonoludek <martynacichosz2@gmail.com>
2022-09-14 01:02:56 +0200

    Richard notices the unknown commands

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ def main():
     intents = discord.Intents.default()
     intents.message_content = True
 
-    bot = commands.Bot(command_prefix='r!', intents=intents, help_command=None)
+    bot = commands.Bot(command_prefix='r!', intents=intents)
+    bot.load_extension('bot')
+
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send("Unknown command")
 
     bot.run(token)
-
     return 0
 
 main()
