9e172943473f19d72c79226f04f5af708c79866c
Nex <awdykowicz.jakub@gmail.com>
2022-09-14 15:52:06 +0200

    Debug

--- announcements_scrapping.py
+++ announcements_scrapping.py
@@ -8,8 +8,11 @@ payload = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.84',
     'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7'
 }
-with requests.Session() as s:
-    assert s.get(base_url, headers=payload), "Connection Failed!\n"
+try:
+    requests.get(base_url, headers=payload)
+    print("Connection Succeded!\n")
+except:
+    print("Connection Failed!\n")
 
 ''' Load Archive & varables assignment '''
 with open("announcements_archive.json", "r") as json_file:
@@ -33,18 +36,17 @@ try:
             soup = BeautifulSoup(response.content, 'lxml')
 
             for link in soup.find('ul', class_='newsgrid-list').find_all('a'):
-                archive['announcements'].append({"page": p, "id": id, "title": link["title"].preetify(), "href": link["href"].preetify(), "img": link.find('img')['src'].preetify(), "content": link.find('div', class_='text').text.preetify()})
+                archive['announcements'].append({"page": p, "id": id, "title": link["title"], "href": link["href"], "img": link.find('img')['src'], "content": link.find('div', class_='text').text})
                 id+=1
-
     print("Page scrapped succesfully!\n")
+
+    ''' Save data to json '''
+    try:
+        with open("announcements_archive.json", "w") as json_file:
+            json.dump(archive, json_file, indent=4)
+        print("Data saved to json succesfully!\n")
+    except:
+        print("During saving data to json, the program encountered a problem!\n")
+
 except:
     print("During scrapping, the program encountered a problem!\n")
-
-''' Save data to json '''
-try:
-    with open("announcements_archive.json", "w") as json_file:
-        json.dump(archive, json_file, indent=4)
-    print("Data save to json succesfully!\n")
-except:
-    print("During saving data to json, the program encountered a problem!\n")
-
