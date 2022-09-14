4f90d9e6028e278a71394fc7bae488b2ca569974
Nex <awdykowicz.jakub@gmail.com>
2022-09-14 16:59:59 +0200

    Encoding fix

--- announcements_scrapping.py
+++ announcements_scrapping.py
@@ -1,6 +1,6 @@
-from re import template
 import requests, json
 from bs4 import BeautifulSoup
+from bs4.dammit import EncodingDetector
 
 ''' Session Initialization '''
 base_url = 'https://www.wojsko-polskie.pl'
@@ -9,10 +9,13 @@ payload = {
     'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7'
 }
 try:
-    requests.get(base_url, headers=payload)
-    print("Connection Succeded!\n")
+    response = requests.get(base_url, headers=payload)
+    http_encoding = response.encoding if 'charset' in response.headers.get('content-type', '').lower() else None
+    html_encoding = EncodingDetector.find_declared_encoding(response.content, is_html=True)
+    encoding = html_encoding or http_encoding
+    print("-> Connection Succeded")
 except:
-    print("Connection Failed!\n")
+    print("-> Connection Failed")
 
 ''' Load Archive & varables assignment '''
 with open("announcements_archive.json", "r") as json_file:
@@ -24,7 +27,7 @@ with requests.Session() as s:
     while True:
         if previous == page: break
         response = s.get(f'{base_url}/wat/articles/list/komunikaty-dla-studentow/?strona={page}', headers=payload)
-        soup = BeautifulSoup(response.content, 'lxml')
+        soup = BeautifulSoup(response.content, 'lxml', from_encoding=encoding)
         previous = page
         page = int(soup.find('div', class_='pagination-wrapper').find_all('a')[-2].text)
 
@@ -33,20 +36,20 @@ try:
     with requests.Session() as s:
         for p in range(page, 0, -1):
             response = s.get(f'{base_url}/wat/articles/list/komunikaty-dla-studentow/?strona={p}', headers=payload)
-            soup = BeautifulSoup(response.content, 'lxml')
+            soup = BeautifulSoup(response.content, 'lxml', from_encoding=encoding)
 
             for link in soup.find('ul', class_='newsgrid-list').find_all('a'):
-                archive['announcements'].append({"page": p, "id": id, "title": link["title"], "href": link["href"], "img": link.find('img')['src'], "content": link.find('div', class_='text').text})
+                archive['announcements'].append({"page": p, "id": id, "title": link["title"], "href": link["href"], "img": link.find('img')['src'].replace('\n', ''), "content": link.find('div', class_='text').text.strip().replace('\n', '')})
                 id+=1
-    print("Page scrapped succesfully!\n")
+    print("-> Page scrapped succesfully")
 
     ''' Save data to json '''
     try:
-        with open("announcements_archive.json", "w") as json_file:
-            json.dump(archive, json_file, indent=4)
-        print("Data saved to json succesfully!\n")
+        with open("announcements_archive.json", "w", encoding='utf8') as json_file:
+            json.dump(archive, json_file, indent=4, ensure_ascii=False)
+        print("-> Data saved to json succesfully")
     except:
-        print("During saving data to json, the program encountered a problem!\n")
+        print("-> During saving data to json, the program encountered a problem")
 
 except:
-    print("During scrapping, the program encountered a problem!\n")
+    print("-> During scrapping, the program encountered a problem")
